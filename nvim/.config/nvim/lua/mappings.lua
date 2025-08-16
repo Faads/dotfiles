74261d003efaddd50fcf326b8a366edc6178b34a
milkman <robin.surell@hotmail.com>
2025-08-16 08:33:12 +0200

    updated

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -8,3 +8,9 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Disable default Tab accept
+vim.g.copilot_no_tab_map = true
+
+-- Accept Copilot suggestion with Ctrl-Space
+vim.api.nvim_set_keymap("i", "<C-1>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+
